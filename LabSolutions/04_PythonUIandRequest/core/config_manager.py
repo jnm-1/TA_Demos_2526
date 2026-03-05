582c3308228f996bc1876d13525b65dc44710556
jnm-1 <161078745+jnm-1@users.noreply.github.com>
2026-03-05 16:06:07 +0100

    JSON config load and read finished

--- LabSolutions/04_PythonUIandRequest/core/config_manager.py
+++ LabSolutions/04_PythonUIandRequest/core/config_manager.py
@@ -2,4 +2,22 @@ import json
 
 CONFIG_FILE = "config.json"
 
-""" We will stub this in as practice """
+def load_config() -> dict:
+    """
+    Loads the user config from a JSON file
+    :return: dictionary of settings or empty dict if not found
+    """
+    try:
+        with open(CONFIG_FILE, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
+def save_config(data_dict: dict) -> None:
+    """
+    Saves a configuration to a local JSON file
+    :param data_dict: dictionary of save data with keys being UI elements
+    :return: None
+    """
+    with open(CONFIG_FILE, "w") as f:
+        json.dump(data_dict, f, indent=4)
